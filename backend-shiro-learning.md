f5f88ad7c02816ae97d1874f12c6adbea501d56a
emon <liming20110711@163.com>
2023-03-14 17:18:57 +0800

    update:20230314-03

--- backend-shiro-learning.md
+++ backend-shiro-learning.md
@@ -5,3 +5,17 @@ Shiro学习：
 Shiro1.9：https://www.bilibili.com/video/BV11e4y1n7BH/?spm_id_from=333.337.search-card.all.click&vd_source=b850b3a29a70c8eb888ce7dff776a5d1
 
 Netty：https://www.bilibili.com/video/BV1DJ411m7NR/?spm_id_from=333.337.search-card.all.click&vd_source=b850b3a29a70c8eb888ce7dff776a5d1
+
+
+
+# 概念整理
+
+| 名称           | 含义                  |                |
+| -------------- | --------------------- | -------------- |
+| authentication | 认证                  | ɔːˌθentɪˈkeɪʃn |
+| authenticator  | 认证者                | ɔːˈθɛntɪkeɪtə  |
+| authorization  | 授权                  | ˌɔːθəraɪˈzeɪʃn |
+| authorizer     | 授权人                |                |
+| principals     | 身份，比如：用户名    | ˈprɪnsəpəlz    |
+| credentials    | 证明/凭证，比如：密码 | krəˈdenʃlz     |
+
---
+++ shiro-springboot/shiro-springboot-01-login/src/main/java/com/coding/shiro/springboot/example01/login/ShiroRun.java
@@ -0,0 +1,38 @@
+package com.coding.shiro.springboot.example01.login;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.*;
+import org.apache.shiro.env.BasicIniEnvironment;
+import org.apache.shiro.env.Environment;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.Subject;
+
+public class ShiroRun {
+    public static void main(String[] args) {
+        // 1.初始化获取SecurityManager
+        /*IniSecurityManagerFactory factory = new IniSecurityManagerFactory("classpath:shiro.ini");
+        SecurityManager securityManager = factory.getInstance();*/
+        Environment environment = new BasicIniEnvironment("classpath:shiro.ini");
+        SecurityManager securityManager = environment.getSecurityManager();
+        SecurityUtils.setSecurityManager(securityManager);
+        // 2.获取Subject对象
+        Subject subject = SecurityUtils.getSubject();
+        // 3.创建token对象，web应用用户名密码从页面传递
+        AuthenticationToken token = new UsernamePasswordToken("emon", "emon123");
+        // 4.完成登录
+        try {
+            subject.login(token);
+            System.out.println("认证结果：" + subject.isAuthenticated());
+            System.out.println("登录成功！");
+        } catch (UnknownAccountException e) {
+            e.printStackTrace();
+            System.out.println("用户不存在！");
+        } catch (IncorrectCredentialsException e) {
+            e.printStackTrace();
+            System.out.println("密码错误！");
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+            System.out.println("登录失败！");
+        }
+    }
+}
